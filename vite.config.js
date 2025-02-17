e8e77d82ff4a0f9fec013ce3bf062dc37480db05
DaPanda03 <77337331+DaPanda03@users.noreply.github.com>
2025-02-17 13:19:51 -0800

    debugging github deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Set correct base path for GitHub Pages
   server: {
     port: 3000,
     strictPort: true,
