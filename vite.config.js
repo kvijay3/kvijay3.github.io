fea65cc83b07a7d5a944c1fa75e5c26e91d9ddc0
DaPanda03 <77337331+DaPanda03@users.noreply.github.com>
2025-02-17 19:45:46 -0800

    Fixed routing and base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Base URL for GitHub Pages
+  base: '/vijaykumar-karthikeyan.github.io/',  // This is crucial for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
-      // Resolve 'crypto' to the 'crypto-browserify' polyfill for Node.js environments
       crypto: 'crypto-browserify',
     },
   },
   define: {
-    // Make 'process.env' available in the build (helps with certain Node.js packages)
     'process.env': {},
-    global: 'window', // Forces the 'global' object to be set to 'window' in Node.js environment
+    global: 'window',
   },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    target: 'esnext', // Ensure compatibility with modern JavaScript features
+    target: 'esnext',
   },
   server: {
     port: 3000
