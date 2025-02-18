c805e1756187cb1cec8849a1c660170710260285
DaPanda03 <77337331+DaPanda03@users.noreply.github.com>
2025-02-17 19:41:57 -0800

    debugging website

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { createTheme } from '@mui/material/styles';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
